6edd4a2a108945b8da63810f0f9263936ecc5e2c
Dev-Mik <pranavlal896@gmail.com>
2023-10-30 18:43:41 +0530

    Dont what was this
    Dont what was this

--- utils/commands/dumpedIPs/getip.py
+++ utils/commands/dumpedIPs/getip.py
@@ -1,7 +1,7 @@
 import json
 from utils.messages.messages import get_message
-protectedlist = ['Tasfiq', 'Farazi106', 'Mikthegmr', 'SathvikGamingYT', 'asg65rip', 'zBe4tTMe', 'FAHIM678', 'woah_noicee', 'MrUniqueXD']
-servers = ["China", "CrownMC", "QrexPvP"]
+protectedlist = ['Tasfiq', 'Farazi106', 'Mikthegmr', 'SathvikGamingYT', 'asg65rip', 'zBe4tTMe', 'FAHIM678', 'woah_noicee', 'MrUniqueXD', '_OpenHacked_']
+servers = ['China', 'CrownMC', 'QrexPvP']
 
 ips = []
 def getip(username):
@@ -10,10 +10,10 @@ def getip(username):
     if username in protectedlist: return get_message('command.ipget.protectedlist')
     for server in servers:
         with open(f'utils/{server}.json', 'r') as f:
-            data = f.read()
+            data = f.read().lower()
             data = json.loads(data)    
-        try: ips.append(f'IP of {username} is {data[username]} and was found in {server}') 
+        try: ips.append(f'IP of {username} is {data[username.lower()]} and was found in {server}') 
         except: continue
     for ip in ips: text = text + ip + '\n'
-    if text == '': text = f'IP of {username} cannot be found'
+    if text == '': text = f'IP of {username} cannot be found. Remember that bedrock account\'s username have to have a \* as prefix'
     return text
